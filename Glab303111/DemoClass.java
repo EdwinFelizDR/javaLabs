ed892748863eb89cd303b5dfa91a686fa5607ac6
EdwinFelizDR <efeliz12lg@gmail.com>
2024-07-16 14:58:51 -0400

    GLAB - 303.11.1 -  Generic Method and Class

---
+++ Glab303111/DemoClass.java
@@ -0,0 +1,9 @@
+package Glab303111;
+
+public class DemoClass {
+    // create a generics method
+    public <T> void genericsMethod(T data) {
+        System.out.println("Generics Method:");
+        System.out.println("Data Passed: " + data);
+    }
+}
